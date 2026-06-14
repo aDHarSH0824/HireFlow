5679bfc68facb0668e1ae89265ce7b112edac572
Harsh Gupta <aharsh0824@gmail.com>
2026-06-14 12:11:59 +0530

    Update config.js for relative routing

--- src/config.js
+++ src/config.js
@@ -1,3 +1,6 @@
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:80/phpdbms/HireWay/hireway/api';
+const API_URL = process.env.REACT_APP_API_URL || 
+  (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
+    ? 'http://localhost:80/phpdbms/HireWay/hireway/api' 
+    : window.location.origin + '/api');
 
 export default API_URL;
